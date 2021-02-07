d970ec4b84e0bba94179196e7a4fa963c9794de8
Gennady  Oleynik <oleygen@MacBook-Pro-Gennady.local>
2021-02-07 14:48:32 +0200

    Remove unused key

--- MovieTestApp/source/AppConstants.h
+++ MovieTestApp/source/AppConstants.h
@@ -12,6 +12,4 @@
 #endif /* AppConstants_h */
 
 #define API_KEY @"eb3cba3e90394a438c8e3b6937d5aaa1"
-#define API_ACCESS_KEY @"eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlYjNjYmEzZTkwMzk0YTQzOGM4ZTNiNjkzN2Q1YWFhMSIsInN1YiI6IjYwMWQ2ZjI0NzMxNGExMDAzZGZlMDA2MSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.v7Ywx5dUhLHfnO9ZF-nffPwm7CUfKpbBp-jZgn8mHBQ"
-
 #define BASE_URL @"https://api.themoviedb.org/3/"
